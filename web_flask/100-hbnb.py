dbe7d37cc150fbb1371a2cccebbda2dfc63c4b19
thebasitguy <mailbasitgh@gmail.com>
2024-05-20 04:41:28 -0700

    errors fixed

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 '''A simple Flask web application.'''
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup
 
 from models import storage
 from models.amenity import Amenity
+from models.place import Place
 from models.state import State
 
 
@@ -12,20 +13,25 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/hbnb_filters')
-def hbnb_filters():
-    '''The hbnb_filters page.'''
+@app.route('/hbnb')
+def hbnb():
+    '''The hbnb page.'''
     all_states = list(storage.all(State).values())
     amenities = list(storage.all(Amenity).values())
+    places = list(storage.all(Place).values())
     all_states.sort(key=lambda x: x.name)
     amenities.sort(key=lambda x: x.name)
+    places.sort(key=lambda x: x.name)
     for state in all_states:
         state.cities.sort(key=lambda x: x.name)
+    for place in places:
+        place.description = Markup(place.description)
     ctxt = {
         'states': all_states,
-        'amenities': amenities
+        'amenities': amenities,
+        'places': places
     }
-    return render_template('10-hbnb_filters.html', **ctxt)
+    return render_template('100-hbnb.html', **ctxt)
 
 
 @app.teardown_appcontext
